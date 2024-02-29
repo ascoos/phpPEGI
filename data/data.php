7bf71257889f05f044e6cbf74b84a2b3ba74a3e3
Ascoos Development <ascoos@users.noreply.github.com>
2024-02-29 23:09:58 +0200

    Initial Version

---
+++ data/data.php
@@ -0,0 +1,90 @@
+<?php 
+/**
+ * Example data.
+ * In real conditions you should have database code to pull the data from.
+ */
+$games = [
+    'pegi' => [
+        '3' => [
+            0 => [
+                'content' => [],
+                'title' => 'Boardwalk Arcade 2',
+                'genre' => '',
+                'description' => 'Boardwalk Arcade 2 is a sequel to the successful Party Arcade game. Boardwalk Arcade 2 offers a brand-new location on the boardwalk featuring exciting and classic mini games.',
+                'developer' => '',
+                'publiser' => 'U&I Entertainment Limited',
+                'platforms' => [
+                    'Nintendo Switch' => '26/01/2024',
+                ],
+                'website' => '',
+                'screen' => ''
+            ],
+        ],
+        '7' => [
+            0 => [
+                'content' => ['Violence', 'Purchases'],
+                'title' => 'Party Animals',
+                'genre' => 'Brawler, party',
+                'description' => '',
+                'developer' => 'Recreate Games',
+                'publiser' => 'SOURCE TECHNOLOGY',
+                'platforms' => [
+                    'Xbox One' => '22/11/2023',
+                    'Xbox Series X|S' => '22/11/2023',
+                    'PC' => '22/11/2023'
+                ],
+                'website' => '',
+                'screen' => ''
+            ],
+        ],
+        '12' => [
+            0 => [
+                'content' => ['Bad'],
+                'title' => 'TEVI',
+                'genre' => 'Action, Adventure, Indie',
+                'description' => 'Embark on an epic action-adventure as you slash, dash, and combo your way through a vast, vibrant world. Explore and uncover hidden secrets. Customize your build to triumph in spectacular boss battles. Experience a tale of mystery, magic, and mayhem in TEVI, a bullet hell metroidvania!',
+                'developer' => 'CreSpirit',
+                'publiser' => 'PM Studios',
+                'platforms' => [
+                    'Nintendo Switch' => '20/02/2024',
+                    'PlayStation 5' => '20/02/2024',
+                ],
+                'website' => 'https://www.pm-studios.com/Projects/tevi',
+                'screen' => 'https://static.wixstatic.com/media/572936_f6c35720dd4b4a1a8a82f38a172974a3~mv2.jpg'
+            ],
+        ],
+        '16' => [
+            0 => [
+                'content' => ['Bad', 'Fear'],
+                'title' => 'Spirit Hunter: Death Mark II',
+                'genre' => 'Horror adventure, visual novel',
+                'description' => 'Spirit Hunter: Death Mark II (known in Japan as Shinigami: Shibito Magire[a]) is a horror visual novel adventure game developed and published by Experience. ',
+                'developer' => 'Experience',
+                'publiser' => 'Aksys Games',
+                'platforms' => [
+                    'Nintendo Switch' => '15/02/2024',
+                    'PlayStation 5' => '15/02/2024',
+                ],
+                'website' => '',
+                'screen' => ''
+            ],
+        ],
+        '18' => [
+            0 => [
+                'content' => ['Bad', 'Violence', 'Gambling'],
+                'title' => 'Fallout New Vegas Ultimate Edition',
+                'genre' => 'Role-playing, Action, Open World',
+                'description' => '',
+                'developer' => '',
+                'publiser' => 'ZeniMax Europe Limited/Bethesda Softworks Europe',
+                'platforms' => [
+                    'PC' => '29/02/2024'
+                ],
+                'website' => '',
+                'screen' => ''
+            ],
+        ]
+    ],
+
+];
+?>
